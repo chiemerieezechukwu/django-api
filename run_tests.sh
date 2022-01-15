e319fddea6a459f0b029fc6837fca43dd1c7ec5d
Chiemerie Ezechukwu <50174206+chiemerieezechukwu@users.noreply.github.com>
2022-01-16 00:19:51 +0100

    :wrench: fixed workflows

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -xe
 
-pytest --cov-report=term-missing --cov=cars/ --disable-network -x --cov-fail-under=90 || exit 1
+pytest --cov-report=term-missing --cov=cars --disable-network -x --cov-fail-under=90 || exit 1
+
+echo "writing coverage..."
+
+coverage json
